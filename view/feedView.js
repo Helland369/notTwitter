fa2b67c92f4deaf0c82810a35add07268911a047
Thomas Helland <thomas.helland97@gmail.com>
2024-10-01 19:41:10 +0200

    a start

---
+++ view/feedView.js
@@ -0,0 +1,29 @@
+//
+//
+//
+function feedView() {
+    main.innerHTML = `
+        <header>
+            <h1>Not Twitter</h1>
+        </header>
+        <nav>
+            <ul>
+                <li>Feed</li>
+                <li>Profile</li>
+                <li>Friends</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+        <div>
+            <div>
+                <p>A post</p>
+            </div>
+            <div>
+                <p>one more post</p>
+            </div>
+            <div>
+                <p>This goes in the midle</p>
+            </div>
+        </div>
+    `;
+}
