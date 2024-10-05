fd7cc6c8f503052be0a83ef399a3f9f7638bd8e9
Thomas Helland <thomas_helland@pm.me>
2024-10-05 21:48:30 +0200

    pasing input check

--- controller/signupController.js
+++ controller/signupController.js
@@ -23,54 +23,58 @@ function signup() {
 // first ["form"] = name/id of form
 // second ["form"] = name/id of input
 
-function signupController() {
-    const form = document.querySelector("#regForm");
-    let fname = document.getElementById("fname").value; //document.forms["regForm"]["fname"].value;
-    let lname = document.getElementById("lname").value; //document.forms["regForm"]["lname"].value;
-    let usrName = document.getElementById("usrName").value; //document.forms["regForm"]["usrName"].value;
-    let email = document.getElementById("email").value; //document.forms["regForm"]["email"].value;
-    let password = document.getElementById("regPassword").value; //document.forms["regForm"]["regPassword"].value;
-    let password2 = document.getElementById("regPassword2").value; //document.forms["regForm"]["regPassword2"].value;
+function signupController(event) {
+    event.preventDefault();
 
-    if (fname !== null || fname !== "") {
-        model.input.usrIn.fname = fname;
-        console.log(model.input.usrIn.fname);
-        return true;
-    } else if (fname === null || fname === "") {
+    const fname = document.getElementById("fname").value; //document.forms["regForm"]["fname"].value;
+    const lname = document.getElementById("lname").value; //document.forms["regForm"]["lname"].value;
+    const usrName = document.getElementById("usrName").value; //document.forms["regForm"]["usrName"].value;
+    const email = document.getElementById("email").value; //document.forms["regForm"]["email"].value;
+    const password = document.getElementById("regPassword").value; //document.forms["regForm"]["regPassword"].value;
+    const password2 = document.getElementById("regPassword2").value; //document.forms["regForm"]["regPassword2"].value;
+
+    if (fname == null || fname == "") {
         alert("You need to enter a First name!");
         return false;
+    } else {
+        model.input.usrIn.fname = fname;
+        console.log(model.input.usrIn.fname);
     }
-    if (lname !== null || lname !== "") {
-        model.input.usrIn.lname = lname;
-        console.log(model.input.usrIn.lname);
-        return true;
-    } else if (lname === null || lname === "") {
+    if (lname == null || lname == "") {
         alert("You need to enter a Last name!");
         return false;
+    } else {
+        model.input.usrIn.lname = lname;
+        console.log(model.input.usrIn.lname);
     }
-    if (usrName !== null || usrName !== "") {
-        model.input.usrIn.usrName == usrName;
-        console.log(model.input.usrIn.usrName);
-        return true;
-    } else if (usrName === null || usrName === "") {
+    if (usrName == null || usrName == "") {
         alert("You need to enter a User name!");
         return false;
+    } else {
+        model.input.usrIn.usrName = usrName;
+        console.log(model.input.usrIn.usrName);
     }
-    if (email !== null || email !== "") {
-        model.input.usrIn.mail = email;
-        console.log(model.input.usrIn.mail);
-        return true;
-    } else if (email === null || email === "") {
+    if (email == null || email == "") {
         alert("You need to enter an email!");
         return false;
+    } else {
+        model.input.usrIn.mail = email;
+        console.log(model.input.usrIn.mail);
     }
-    if (password === password2) {
-        model.input.usrIn.password = password;
-        console.log(model.input.usrIn.password);
-        return true;
-    } else if (password !== password2 || password2 !== password) {
+    if (password !== password2) {
         alert("You need to enter a password");
         return false;
+    } else {
+        model.input.usrIn.password = password;
+        console.log(model.input.usrIn.password);
     }
-    form.submit();
+
+    model.usr[0].fname = model.input.usrIn.fname;
+    model.usr[0].lname = model.input.usrIn.lname;
+    model.usr[0].usrName = model.input.usrIn.usrName;
+    model.usr[0].mail = model.input.usrIn.mail;
+    model.usr[0].password = model.input.usrIn.password;
+    //form.submit();
+
+    return true;
 }
--- view/signupView.js
+++ view/signupView.js
@@ -4,21 +4,27 @@
 function signupView() {
     main.innerHTML = `
         ${headerView()}
-        <form id="regForm" name="regForm">
+        <form id="regForm" name="regForm" onsubmit="signupController(event)">
             <h1>Create an account</h1>
             <label for="fname">First Name:</label><br>
-            <input type="text" name="fname" id="fname" placeholder="First name" autocomplete="off"><br>
+            <input type="text" id="fname" placeholder="First name" autocomplete="off"><br>
+
             <label for="lname">Last Name:</label><br>
-            <input type="text" name="lname" id="lname" placeholder="Last name" autocomplete="off"><br>
+            <input type="text" id="lname" placeholder="Last name" autocomplete="off"><br>
+
             <label for="usrName">User Name:</label><br>
-            <input type="text" name="usrName" id="usrName" placeholder="User name" autocomplete="off"><br>
+            <input type="text" id="usrName" placeholder="User name" autocomplete="off"><br>
+
             <label for="email">Email:</label><br>
-            <input type="email" name="email" id="email" placeholder="Email" autocomplete="off"> <br>
+            <input type="email" id="email" placeholder="Email" autocomplete="off"> <br>
+
             <label for="regPassword">Password</label> <br>
-            <input type="password" name="regPassword" id="regPassword" placeholder="password" autocomplete="off"> <br>
+            <input type="password" id="regPassword" placeholder="password" autocomplete="off"> <br>
+
             <label for="regPassword2">Re-enter password</label> <br>
-            <input type="password" name="regPassword2" id="regPassword2" placeholder="Re-enter password" autocomplete="off"> <br><br>
-            <input type="submit" value="Create account" onsubmit="signupController()">
+            <input type="password" id="regPassword2" placeholder="Re-enter password" autocomplete="off"> <br><br>
+
+            <input type="submit" value="Create account">
             <br><br><br>
         </form>
         ${footerView()}
