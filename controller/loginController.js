d3d533c058bf13f610ad4f50712ec3cd0ed20966
Thomas Helland <thomas_helland@pm.me>
2024-10-10 18:14:16 +0200

    Added login function

--- controller/loginController.js
+++ controller/loginController.js
@@ -5,3 +5,25 @@ function login() {
     model.app.page = "login";
     updateView();
 }
+
+function loginController(event) {
+    event.preventDefault();
+
+    const nameIn = document.getElementById("usrLogin").value;
+    const passIn = document.getElementById("usrPassword").value;
+    const usrName = model.usr[0].usrName;
+    const usrPass = model.usr[0].password;
+    let count = 3;
+
+    if (nameIn === usrName && passIn === usrPass) {
+        model.app.page = "feed";
+        updateView();
+        return true;
+    } else if (nameIn === null || nameIn === "") {
+        alert("Wrong user name!");
+    } else if (passIn === null || nameIn === "") {
+        alert("Wrong password");
+    }
+
+    return false;
+}
